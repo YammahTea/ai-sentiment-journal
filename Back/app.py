cd425c6108df2766370bf8c20880888c1c01c4d4
clocky <clockyTestEmail@gmail.com>
2025-11-25 00:56:18 +0300

    Added sentiment model

--- Back/app.py
+++ Back/app.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from Back.text import classify_message
 
 app = FastAPI()
+
+class JournalEntry(BaseModel):
+  text:str
 
 @app.get("/messages")
 def messages() -> dict[str, bool | str]:
   return {"Retrieve": True, "Message": "some messages"}
 
 @app.post("/journal")
-def analyze_message(text:str) -> str:
-  # mode = classify_message(text)
-  mode = "positive"
-  return mode
-
+def analyze_message(entry: JournalEntry):
+  try:
+    mode, score = classify_message(entry.text)
+    return mode, score
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
 
--- Back/text.py
+++ Back/text.py
@@ -1,6 +1,31 @@
+from textblob import TextBlob
+
+#Recives users message, save message to db, analyze the message with TextBlob
+# save message score to db, return mode based on message score
 def classify_message(message):
-    #mode = logisticReg(message)
-    mode = "do classification"
-    return mode
+    testimonial = TextBlob(message)
+    mode_score = testimonial.sentiment.polarity
+
+    mode: str
+    if mode_score <= -0.5:
+      mode = "Very Negative"
+    elif mode_score < 0:
+      mode = "Negative"
+    elif mode_score == 0:
+      mode = "Neutral"
+    elif mode_score < 0.5:
+      mode = "Positive"
+    else:
+      mode = "Very Positive"
+
+    """ ˅˅˅ TO DO ˅˅˅ """
+    # save message to db
+    # save mode_score to db
+    # save final sentiment to db
+    """ ^^^ TO DO ^^^ """
+
+    return mode, mode_score
+
+
 
 
